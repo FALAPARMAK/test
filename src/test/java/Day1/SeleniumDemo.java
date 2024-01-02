d370db3fd7b35b9e13ae679976961591cda760f0
FALAPARMAK <betulalaparmak1@gmail.com>
2024-01-01 23:06:50 -0600

    First Commit

---
+++ src/test/java/Day1/SeleniumDemo.java
@@ -0,0 +1,48 @@
+package Day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chromium.ChromiumDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.List;
+import java.util.Set;
+
+public class SeleniumDemo {
+
+    public static void main(String[] args) {
+
+
+        System.setProperty("webdriver.chrome.driver","/Users/mehmetalaparmak/Downloads/geckodriver");
+       // System.setProperty("webdriver.chrome.driver","/Users/mehmetalaparmak/Downloads/chromedriver_mac64 2/chromedriver");
+
+
+
+        WebDriver driver=new FirefoxDriver();
+        //WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("http://facebook.com");
+        driver.findElement(By.name("email")).sendKeys("4694944850");
+        driver.findElement(By.id("pass")).sendKeys("Betul1980");
+         WebElement login=  driver.findElement(By.name("login"));
+         login.click();
+       // driver.get("http://google.com");
+
+//
+//        JavascriptExecutor js=(JavascriptExecutor)driver;
+//        js.executeScript("window.scrollBy(0,4000)");
+//
+//        driver.findElement(By.cssSelector("#fname"));
+//
+//        driver.findElement(By.id("login"));
+        driver.quit();
+
+
+
+
+    }
+}
